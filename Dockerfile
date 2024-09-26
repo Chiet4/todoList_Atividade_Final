cb910f1256f270cebd41bebca41d66c15115b8e8
Anchieta N. Albano <anchietaalbano@alu.ufc.br>
2024-09-26 14:27:05 -0300

    add connection com RDS

--- Dockerfile
+++ Dockerfile
@@ -4,8 +4,9 @@ WORKDIR /app
 
 COPY requirements.txt requirements.txt
 
-RUN mkdir /app/todolist
+RUN apk add --no-cache postgresql-dev gcc python3-dev musl-dev
 
+RUN mkdir /app/todolist
 RUN mkdir /app/todolist/templates
 
 COPY wsgi.py wsgi.py
@@ -13,7 +14,6 @@ COPY wsgi.py wsgi.py
 RUN pip install -r requirements.txt
 
 COPY __init__.py todolist/__init__.py
-
 COPY base.html todolist/templates/base.html
 
-CMD [ "gunicorn", "--workers", "2",  "--bind", "0.0.0.0", "wsgi:app" ]
+CMD [ "gunicorn", "--workers", "2",  "--bind", "0.0.0.0:8000", "wsgi:app" ]
--- __init__.py
+++ __init__.py
@@ -1,13 +1,20 @@
+import os
 from flask import Flask, render_template, request, redirect, url_for 
 from flask_sqlalchemy import SQLAlchemy
 
-app = Flask(__name__,template_folder='templates')
+app = Flask(__name__, template_folder='templates')
 
-# /// = relative path, //// = absolute path
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite'
+# Configuração para o RDS PostgreSQL
+db_username = os.environ.get('DB_USERNAME')
+db_password = os.environ.get('DB_PASSWORD')
+db_host = os.environ.get('DB_HOST')
+db_port = os.environ.get('DB_PORT')
+db_name = os.environ.get('DB_NAME')
+
+app.config['SQLALCHEMY_DATABASE_URI'] = f'postgresql://{db_username}:{db_password}@{db_host}:{db_port}/{db_name}'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-db = SQLAlchemy(app)
 
+db = SQLAlchemy(app)
 class Todo(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(100))
